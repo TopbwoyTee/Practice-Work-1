46d653131eab6756323ab6ab9a1bd2489b2a0797
Topbwoy <kawedot@gmail.com>
2024-07-20 07:12:39 -0400

    init commit

---
+++ app/src/main/java/com/tobe/class1a/MainActivity2.kt
@@ -0,0 +1,19 @@
+package com.tobe.class1a
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    var count = 1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+
+    }
+}
